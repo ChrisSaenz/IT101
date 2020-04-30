69bf800691ee0e3b8fdee5c97b73fb15da2ae62d
Chris Saenz <chris@saenz.ca>
2020-04-30 00:50:45 -0700

    email

---
+++ email.ps1
@@ -0,0 +1,14 @@
+﻿$From = "vincboys@gmail.com"
+$To = "chris@saenz.ca"
+$Subject = "Test Message"
+$Body = "
+Hello $From
+THis is an email
+from me?
+-goodbye
+
+"
+$SMTPServer = "smtp.gmail.com"
+$SMTPPort = "587"
+
+Send-MailMessage -From $From -to $To -Subject $Subject -Body $Body -SmtpServer $SMTPServer -port $SMTPPort -UseSsl -Credential (Get-Credential)
