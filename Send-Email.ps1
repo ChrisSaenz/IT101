dd5a923657285557988085bc2aa1b9d25f02e60c
Chris Saenz <chris@saenz.ca>
2020-04-30 01:15:06 -0700

    email

---
+++ Send-Email.ps1
@@ -0,0 +1,20 @@
+﻿#### Build Credential Object
+$password = ConvertTo-SecureString 'test' -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ('vincboys@gmail.com', $password)
+
+### Build Email
+$From = "vincboys@gmail.com"
+$To = "chris@saenz.ca"
+$Subject = "Test Message"
+$Body = "
+Hello $From
+THis is an email
+from me?
+-goodbye
+
+"
+$SMTPServer = "smtp.gmail.com"
+$SMTPPort = "587"
+
+# Send Email
+Send-MailMessage -From $From -to $To -Subject $Subject -Body $Body -SmtpServer $SMTPServer -port $SMTPPort -UseSsl -Credential $credential
